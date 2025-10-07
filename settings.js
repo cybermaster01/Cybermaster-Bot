025a4a1020149591c6ccc46e181e11876ab66daf
SNOWBIRD XMD <praiseluke47@gmail.com>
2025-10-07 09:28:36 +0200

    Add files via upload

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'Knight Bot',
+  packname: 'ᴄʏʙᴇʀᴍᴀsᴛᴇʀ xᴍᴅ',
   author: '‎',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '919876543210', //Set your number here without + symbol, just add country code & number without any space
+  botName: "ᴄʏʙᴇʀᴍᴀsᴛᴇʀ xᴍᴅ",
+  botOwner: 'CYBERMASTER', // Your name
+  ownerNumber: '263718990351', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   maxStoreMessages: 20, 
